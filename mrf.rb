c92024fcaf05bac2e6cfd88adb46843a4d53d4b4
Kerli Low <kerlilow@gmail.com>
2020-09-06 21:24:30 +0900

    Update to 0.1.1

--- mrf.rb
+++ mrf.rb
@@ -1,14 +1,14 @@
 class Mrf < Formula
   desc "Rename files by pattern matching."
   homepage "https://github.com/kerlilow/mrf"
-  version "0.1.0"
+  version "0.1.1"
 
   if OS.mac?
-    url "https://github.com/kerlilow/mrf/releases/download/v0.1.0/mrf-v0.1.0-x86_64-apple-darwin.tar.gz"
-    sha256 "fcd8ea24cf91b524ab3d9a689053a0e3430fe72e559d07506577f5cb59a5e6b1"
+    url "https://github.com/kerlilow/mrf/releases/download/v0.1.1/mrf-v0.1.1-x86_64-apple-darwin.tar.gz"
+    sha256 "134352d558cc1bf306abf6eb5ad6251454e8641afba5a0d28b3ac71471ed9bf5"
   elsif OS.linux?
-    url "https://github.com/kerlilow/mrf/releases/download/v0.1.0/mrf-v0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "75e67a14c216fc9a97e0c763cd911a62a1b65dc1f175ce93794ea76f4a11782d"
+    url "https://github.com/kerlilow/mrf/releases/download/v0.1.1/mrf-v0.1.1-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "aa3274ad28dfcb44ed42e8a455a00338d32b4e2a7ea7f1d0372bc7d72e03fb8b"
   end
 
   def install
